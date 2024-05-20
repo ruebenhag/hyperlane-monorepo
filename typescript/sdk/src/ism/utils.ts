0828aa440e4fe0b6d8eb24d81325ded9ebd0024c
Paul Balaji <paul@hyperlane.xyz>
2024-05-20 19:13:38 +0100

    tests part 2

--- typescript/sdk/src/ism/utils.ts
+++ typescript/sdk/src/ism/utils.ts
@@ -173,8 +173,14 @@ export async function moduleMatchesConfig(
   // Less stringent checks on Routing/Pausable ISMs if not configured yet
   configured = true,
 ): Promise<boolean> {
+  // Handle custom ISM addresses
   if (typeof config === 'string') {
     return eqAddress(moduleAddress, config);
+  }
+
+  // Handle undefined domains
+  if (!config || !config.type) {
+    return false;
   }
 
   // If the module address is zero, it can't match any object-based config.
@@ -223,6 +229,13 @@ export async function moduleMatchesConfig(
         provider,
       );
 
+      // if fallback routing, check that mailbox matches
+      if (config.type === IsmType.FALLBACK_ROUTING) {
+        const client = MailboxClient__factory.connect(moduleAddress, provider);
+        const mailboxAddress = await client.mailbox();
+        matches &&= mailbox !== undefined && eqAddress(mailboxAddress, mailbox);
+      }
+
       // Check that the RoutingISM owner matches the config
       const owner = await routingIsm.owner();
       // If the ISM is already configured, the owner should match the config
@@ -251,6 +264,11 @@ export async function moduleMatchesConfig(
 
         // Check that the modules for each domain match the config
         for (const domain of onChainDomains) {
+          // skip domains not in the multiprovider
+          if (!multiProvider.hasChain(domain)) {
+            continue;
+          }
+
           const onChainModule = await routingIsm.module(domain);
           const configModule =
             config.domains[multiProvider.getChainName(domain)];
@@ -367,7 +385,6 @@ export async function routingModuleDelta(
   multiProvider: MultiProvider,
   contracts: HyperlaneContracts<ProxyFactoryFactories>,
   mailbox?: Address,
-  configured = true,
 ): Promise<RoutingIsmDelta> {
   const provider = multiProvider.getProvider(destination);
   const routingIsm = DomainRoutingIsm__factory.connect(moduleAddress, provider);
@@ -423,7 +440,6 @@ export async function routingModuleDelta(
         multiProvider,
         contracts,
         mailbox,
-        configured,
       );
       if (!subModuleMatches) delta.domainsToEnroll.push(originDomain);
     }
