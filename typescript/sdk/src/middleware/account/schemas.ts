c96fbb2c2ffd3c12a4302f0a7d2e77aa31310c2c
Noah Bayindirli 🥂 <noah@primeprotocol.xyz>
2024-05-21 18:09:50 -0400

    chore(sdk): add further zod support to SDK

--- typescript/sdk/src/middleware/account/schemas.ts
+++ typescript/sdk/src/middleware/account/schemas.ts
@@ -2,6 +2,10 @@ import { z } from 'zod';
 
 import { ZHash } from '../../index.js';
 import { ZChainName } from '../../metadata/customZodTypes.js';
+import {
+  BigNumberSchema,
+  CallDataSchema,
+} from '../../providers/transactions/schemas.js';
 
 export const AccountConfigSchema = z.object({
   origin: ZChainName,
@@ -9,4 +13,12 @@ export const AccountConfigSchema = z.object({
   localRouter: ZHash.optional(),
   routerOverride: ZHash.optional(),
   ismOverride: ZHash.optional(),
+});
+
+export const GetCallRemoteSettingsSchema = z.object({
+  chain: ZChainName,
+  destination: ZChainName,
+  innerCalls: z.array(CallDataSchema),
+  config: AccountConfigSchema,
+  hookMetadata: BigNumberSchema.optional(),
 });
